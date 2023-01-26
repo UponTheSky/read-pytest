5e524a6823058bc3642a7837876167db074786b5
UponTheSky <sailing.to.orbis@gmail.com>
2023-01-27 08:49:54 +0900

    ch10: finished

--- code/exercises/ch10/test_my_info.py
+++ code/exercises/ch10/test_my_info.py
@@ -3,10 +3,13 @@ import my_info
 
 
 def test_my_home_returns_correct_value():
-    value = my_info.home_dir()
-    assert value == "/users/fake_user"
+    with mock.patch.object(my_info, 'home_dir', return_value='/users/fake_user'):
+        value = my_info.home_dir()
+        assert value == "/users/fake_user"
 
 
 def test_my_home_is_called():
-    my_info.home_dir()
+    with mock.patch.object(my_info.Path, 'home') as mocked_home:
+        my_info.home_dir()
+        mocked_home.assert_called_once()
     # check to see if Path.home() was called
